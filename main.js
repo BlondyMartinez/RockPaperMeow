95776df39cb8042cdec8c2b518e9d30e08423448
Blondy <blondymartinezm@gmail.com>
2024-02-26 18:48:16 +0100

    added paws switch

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ let playerScore = 0;
 let computerScore = 0;
 
 const CHOICES = ["rock", "paper", "meow"];
+const container = document.getElementById('game-output-container');
+const score = document.getElementById('score');
+const computerPaw = document.getElementById('computer-paw');
+const playerPaw = document.getElementById('player-paw');
 
 //gets a random choice for the computer
 function getComputerChoice(){
@@ -11,19 +15,19 @@ function getComputerChoice(){
 
 //adds a <p> to the top of the container given the text content and color
 function addText(content, color) {
-    let container = document.getElementById('game-output-container');
-
     container.textContent = content;
     container.style.color = color;
 }
 
 function updateScore(){
-    let score = document.getElementById('score');
     score.textContent = `${playerScore} | ${computerScore}`;
 }
 
 //returns wether the player has tied, won or lost the round and the winning hand
 function playRound(pChoice, cChoice) {
+    updateComputerPaw(cChoice);
+    updatePlayerPaw(pChoice);
+    
     if (pChoice === cChoice) {
         return "You tie this round!";
     }
@@ -37,6 +41,34 @@ function playRound(pChoice, cChoice) {
     } else {
         computerScore++;
         return `You lose this round! ${cChoice} beats ${pChoice}.`;
+    }
+}
+
+function updateComputerPaw(cChoice){
+    switch(cChoice){
+        case "rock":
+            computerPaw.src = 'resources/images/cat_paw_rock_r.png';
+            break;
+        case "paper":
+            computerPaw.src = 'resources/images/cat_paw_paper_r.png';
+            break;
+        case "meow":
+            computerPaw.src = 'resources/images/cat_paw_meow_r.png';
+            break;
+    }
+}
+
+function updatePlayerPaw(pChoice){
+    switch(pChoice){
+        case "rock":
+            playerPaw.src = 'resources/images/cat_paw_rock.png';
+            break;
+        case "paper":
+            playerPaw.src = 'resources/images/cat_paw_paper.png';
+            break;
+        case "meow":
+            playerPaw.src = 'resources/images/cat_paw_meow.png';
+            break;
     }
 }
 
